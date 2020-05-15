cb759c6b4caa2ae4bc393b82481ee8655f77d29f
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-14 20:12:40 -0600

    Added actions results

--- FightingGames/Controllers/FightingController.cs
+++ FightingGames/Controllers/FightingController.cs
@@ -7,6 +7,11 @@ namespace AspNetCoreTodo.Controllers
 {
     public class FightingController : Controller
     {
-        
+        public IActionResult Index()
+        {
+            // Get to-do items from database
+            // Put items into a model
+            // Render view using the model
+        }
     }
 }
