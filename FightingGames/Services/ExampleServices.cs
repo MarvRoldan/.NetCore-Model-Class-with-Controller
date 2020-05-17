6c2c1812619e6ae8215c6f7d2479d8d360e1329a
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-16 22:35:51 -0600

    Filled in example services

---
+++ FightingGames/Services/ExampleServices.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FightingGames.Models;
+
+namespace FightingGames.Services
+{
+    public class ExampleService : FGamersService
+    {
+        public Task<FGDetails[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new FGDetails
+            {
+                Title = "Street Fighter III Third Strike",
+                SubGenre = "2D",
+                Difficulty = "Hard"
+            };
+            var item2 = new FGDetails
+            {
+                Title = "Granblue Fantasy Versus",
+                SubGenre = "Anime 2D",
+                Difficulty = "Easy"
+            };
+            return Task.FromResult(new[] { item1, item2 });
+        }
+    }
+}
